9f217a46f8c21956142171a0f79ab21f8cc09d8a
androshalikashvili <andro.shalikashvili@gmail.com>
2025-03-17 17:47:10 +0400

    Add Category Create page

--- MVCEcommerce/Controllers/CategoryController.cs
+++ MVCEcommerce/Controllers/CategoryController.cs
@@ -16,5 +16,19 @@ namespace MVCEcommerce.Controllers
             List<Category> objCategoryList = _context.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _context.Categories.Add(obj);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
