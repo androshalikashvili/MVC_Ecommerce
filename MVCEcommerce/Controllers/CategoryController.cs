c1ffbd0be668001086a24c8242c621f8ff51e414
andro shalikashvili <andro.shalikashvili@gmail.com>
2025-03-17 15:41:58 +0400

    11

---
+++ MVCEcommerce/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCEcommerce.Data;
+using MVCEcommerce.Models;
+
+namespace MVCEcommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
