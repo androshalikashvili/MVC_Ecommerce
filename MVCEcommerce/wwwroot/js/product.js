93b19de593402ea894c857cae409dee5a415e009
androshalikashvili <andro.shalikashvili@gmail.com>
2025-03-26 17:18:58 +0400

    Add Product Details Page

--- MVCEcommerce/wwwroot/js/product.js
+++ MVCEcommerce/wwwroot/js/product.js
@@ -1,17 +1,28 @@
 ﻿var dataTable;
 
-$(function () {
+document.addEventListener('DOMContentLoaded', function () {
     loadDataTable();
 });
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/product/getall'},
+        "ajax": { url: '/admin/product/getall' },
         columns: [
-            { data: 'name', "width": "15%" },
-            { data: 'description', "width": "15%" },
-            { data: 'price', "width": "15%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'name', width: "25%" },
+            { data: 'description', width: "26%" },
+            { data: 'price', width: "12%" },
+            { data: 'category.name', width: "12%" },
+            {
+                data: 'id',
+                width: "25%",
+                render: function (data) {
+                    return `
+                    <div class="w-75 btn-group" role="group">
+                        <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                        <a onClick="Delete('/admin/product/delete/${data}')" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> Delete</a>                        
+                    </div>`;
+                }
+            }
         ]
     });
 }
